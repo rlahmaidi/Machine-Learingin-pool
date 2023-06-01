a6de975c644cf77a8e2491e9f72349c5ec0e7fcd
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-01 04:54:35 +0100

    peeeercentile

--- Module__00/ex01/TinyStatistician.py
+++ Module__00/ex01/TinyStatistician.py
@@ -1,12 +1,13 @@
 import numpy as np
 import sys
+from typing import Union, List
 
 
 class TinyStatistician:
     def __init__(self):
         pass
-    
-    def mean(self,x):
+
+    def mean(self, x):
         if not isinstance(x, (list, np.ndarray)):
             print('Error: function only accept a list or a numpy array')
             return None
@@ -20,7 +21,7 @@ class TinyStatistician:
                 sum += elment
         return sum / len(x)
 
-    def median(self,x): 
+    def median(self, x):
         if not isinstance(x, (list, np.ndarray)):
             print('Error: function only accept a list or a numpy array')
             return None
@@ -34,8 +35,8 @@ class TinyStatistician:
         else:
             x = sorted(x)
             return float(x[int(len(x) / 2)])
-        
-    def quartile(self,x):
+
+    def quartile(self, x):
         if not isinstance(x, (list, np.ndarray)):
             print('Error: function only accept a list or a numpy array')
             return None
@@ -50,34 +51,70 @@ class TinyStatistician:
             first = sort[:int(len(sort) / 2)]
             third = sort[int(len(sort) / 2):]
         else:
-            print("the sort is", sort)
             first = sort[:int(len(sort) / 2) + 1]
             third = sort[int(len(sort) / 2):]
-            print("is the median included in third", third)
-            print("this is first", first)
-            # first = sort[:int(len(sort) / 2)]
-            # third = sort[int(len(sort) / 2) + 1:]
-            # print("the len(sort) is ", len(sort))
-            # print("the first data is ", first)
-            # print("the third data is ", third)
-        first_quartile = self.median(first) 
+        first_quartile = self.median(first)
         third_quartile = self.median(third)
         return[first_quartile, third_quartile]
 
+    # def percentile(self,x:Union[np.ndarray, list], p:int) -> float:
+    #     """ Extracts the percentile p of each features (column of x)
+    #     Parameters:
+    #         x [np.ndarray]: np.ndarray containing the differents features along axis 1.
+    #     Return:
+    #         v_percent [np.ndarray]: array (shape[1, m]) containing the p percentile of each features.
+    #     """
+    #     try:
+    #         l_x = len(x)
+    #         x_sorted = sorted(x)
+    #         idx = int(np.ceil(0.01 * p * l_x)) - 1
+    #         percentiles = x_sorted[idx]
+    #         return percentiles
+    #     except:
+    #         return None
 
-        # if len(sort) % 4 != 0:
-        #     first_quartile = sort[int(len(sort) / 4) - 1]
-        # else:
-        #     first_quartile = (sort[int(len(sort) / 4) - 1] + sort[int(len(sort) / 4)]) / 2
-        # if len(sort) * 3 % 4 == 0:
-        #     third_quartile = sort[l - 1]
-        # else:
-        #     print("number of element is ", len(sort))
-        #     # print("3/4 of it is ", len(sort) * 3 / 4)
-        #     # print("third quartile ", sort[int(l) -1], sort[int(l)])
-        #     # print("their indices are:", int(l) - 1, int(l))
-        #     third_quartile = (sort[int(l)- 1] + sort[int(l)]) / 2
-        return [first_quartile, third_quartile]
+    # def percentile(self, x: list, percentile: int) -> float:
+    #     x = sorted(x)
+    #     m = len(x) - 1
+    #     index: float = m * percentile / 100
+
+    #     if index.is_integer():
+    #         return x[int(index)]
+    #     floored, ceiled = int(index), int(index) + 1
+    #     return x[floored] * (ceiled - index) + x[ceiled] * (index - floored)
+
+    def percentile(self,data, rank):
+        ''' Calculate percentile by linear interpolation between closest ranks: 
+        
+        - If the rank is lower or higher than the minimum or maximum possible ranks,
+        one of data borders will be returned.
+        - If the rank falls between two candidate ranks, linear interpolation is
+        used to calculate the result.
+        
+        More info: http://en.wikipedia.org/wiki/Percentile
+        
+        Example usage: 
+            percentile_linintpol([1,2,3,4,1,5,8], 15)
+        
+        :param data: List or tuple of numeric values (int, float)
+        :param rank: Rank to calculate (values shall be from 0 - 100)
+        '''
+        
+        data = sorted(data)
+        dlen = len(data)
+        ranks = [ (100.0/dlen)*(d - 0.5) for d in range(1, dlen + 1) ]
+        
+        # Speed up corner cases
+        if rank < ranks[0]:  return data[0]
+        if rank > ranks[-1]: return data[-1]
+        
+        for idx in range(0, dlen - 1):
+            rcur, rnext = ranks[idx], ranks[idx + 1]
+            if rcur == rank: 
+                return data[idx]
+            elif rcur < rank and rank < rnext:
+                return data[idx] + dlen*(rank-rcur)*(data[idx+1]-data[idx])/100.0
+
 if __name__ == "__main__":
     # print("@@@@@@@@@@@@ mean @@@@@@@@@")
     # lst = [1,2,3]
@@ -89,15 +126,21 @@ if __name__ == "__main__":
     # array = np.array([13, 16, 23, 26, 26, 35, 35, 37]) #[1,1,2,4]
     # print(TinyStatistician().median(lst))
     # print(np.median(array))
-    print("@@@@@@@@@@@@@ quaritles @@@@@@")
-    odd =  [1, 42, 300, 10, 59]
-    even = [1, 42, 300, 10, 59,60]
-    odd_array = np.array([1, 42, 300, 10, 59])
-    even_array = np.array([1, 42, 300, 10, 59, 60])
-    print("######odd number of data #####")
-    print(TinyStatistician().quartile(odd))
-    print(TinyStatistician().quartile(odd_array))
-    print("######### even number of data #####")
-    print(TinyStatistician().quartile(even))
+    # print("@@@@@@@@@@@@@ quaritles @@@@@@")
+    # odd =  [1, 42, 300, 10, 59]
+    # even = [1, 42, 300, 10, 59,60]
+    # odd_array = np.array([1, 42, 300, 10, 59])
+    # even_array = np.array([1, 42, 300, 10, 59, 60])
+    # print("######odd number of data #####")
+    # print(TinyStatistician().quartile(odd))
+    # print(TinyStatistician().quartile(odd_array))
+    # print("######### even number of data #####")
+    # print(TinyStatistician().quartile(even))
     # print(TinyStatistician().quartile(even_array))
     # print("correcto result is",array.quaritles())
+    print("@@@@@@@@@@@@  percentile @@@@@@@@@@@")
+    a =  [1, 42, 300, 10, 59]
+
+    array = np.array(a)
+    # print(np.percentile(array,20))
+    print(TinyStatistician().percentile(a,20))
--- Module__00/ex01/test.py
+++ Module__00/ex01/test.py
@@ -1,7 +1,10 @@
 import numpy as np
+from math import ceil
 
 x = np.array([1, 2,4,3,2])
 lst = [1,3,4,2,6,5]
-print(sorted(x))
+lst2 = [7,8,9]
+# result = Union(lst,lst2)
+print(ceil(1.2))
 
 
