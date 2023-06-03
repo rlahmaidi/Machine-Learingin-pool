e4e22c74940a8614497557592fd1ded7bd5baaf9
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-03 02:16:19 +0100

    the percentile is fixed

--- Module__00/ex01/TinyStatistician.py
+++ Module__00/ex01/TinyStatistician.py
@@ -57,64 +57,30 @@ class TinyStatistician:
         third_quartile = self.median(third)
         return[first_quartile, third_quartile]
 
-    # def percentile(self,x:Union[np.ndarray, list], p:int) -> float:
-    #     """ Extracts the percentile p of each features (column of x)
-    #     Parameters:
-    #         x [np.ndarray]: np.ndarray containing the differents features along axis 1.
-    #     Return:
-    #         v_percent [np.ndarray]: array (shape[1, m]) containing the p percentile of each features.
-    #     """
-    #     try:
-    #         l_x = len(x)
-    #         x_sorted = sorted(x)
-    #         idx = int(np.ceil(0.01 * p * l_x)) - 1
-    #         percentiles = x_sorted[idx]
-    #         return percentiles
-    #     except:
-    #         return None
+    def percentile(self, x, p):
+        if not isinstance(x, (list, np.ndarray)):
+            print('Error: function only accept a list or a numpy array')
+            return None
+        elif isinstance(x, list) and len(x) == 0:
+            return None
+        elif isinstance(x, np.ndarray) and x.size == 0:
+            return None
+        elif not isinstance(p,(int, float)) or p > 100 or p < 0:
+            print("the percentile you want to calculate should be betwen 0 and 100")
+        else:
+            N = len(x)
+            x = sorted(x)
+            index = (N - 1) *p / 100
+            if index.is_integer():
+                return float(x[int(index)])
+            floored_index = int(index)
+            ceiled_index = int(index) + 1
+            floored_value = x[floored_index]
+            ceiled_value = x[ceiled_index]
+            return float(floored_value + (index - floored_index) *(ceiled_value - floored_value))
 
-    # def percentile(self, x: list, percentile: int) -> float:
-    #     x = sorted(x)
-    #     m = len(x) - 1
-    #     index: float = m * percentile / 100
 
-    #     if index.is_integer():
-    #         return x[int(index)]
-    #     floored, ceiled = int(index), int(index) + 1
-    #     return x[floored] * (ceiled - index) + x[ceiled] * (index - floored)
-
-    def percentile(self,data, rank):
-        ''' Calculate percentile by linear interpolation between closest ranks: 
-        
-        - If the rank is lower or higher than the minimum or maximum possible ranks,
-        one of data borders will be returned.
-        - If the rank falls between two candidate ranks, linear interpolation is
-        used to calculate the result.
-        
-        More info: http://en.wikipedia.org/wiki/Percentile
-        
-        Example usage: 
-            percentile_linintpol([1,2,3,4,1,5,8], 15)
-        
-        :param data: List or tuple of numeric values (int, float)
-        :param rank: Rank to calculate (values shall be from 0 - 100)
-        '''
-        
-        data = sorted(data)
-        dlen = len(data)
-        ranks = [ (100.0/dlen)*(d - 0.5) for d in range(1, dlen + 1) ]
-        
-        # Speed up corner cases
-        if rank < ranks[0]:  return data[0]
-        if rank > ranks[-1]: return data[-1]
-        
-        for idx in range(0, dlen - 1):
-            rcur, rnext = ranks[idx], ranks[idx + 1]
-            if rcur == rank: 
-                return data[idx]
-            elif rcur < rank and rank < rnext:
-                return data[idx] + dlen*(rank-rcur)*(data[idx+1]-data[idx])/100.0
-
+    
 if __name__ == "__main__":
     # print("@@@@@@@@@@@@ mean @@@@@@@@@")
     # lst = [1,2,3]
@@ -142,5 +108,5 @@ if __name__ == "__main__":
     a =  [1, 42, 300, 10, 59]
 
     array = np.array(a)
-    # print(np.percentile(array,20))
-    print(TinyStatistician().percentile(a,20))
+    print("numpy: ",np.percentile(array,100))
+    print("mine",TinyStatistician().percentile(array,100))
--- Module__00/ex01/test.py
+++ Module__00/ex01/test.py
@@ -1,10 +1,16 @@
-import numpy as np
-from math import ceil
+# import numpy as np
+# from math import ceil
 
-x = np.array([1, 2,4,3,2])
-lst = [1,3,4,2,6,5]
-lst2 = [7,8,9]
-# result = Union(lst,lst2)
-print(ceil(1.2))
+# x = np.array([1, 2,4,3,2])
+# lst = [1,3,4,2,6,5]
+# lst2 = [7,8,9]
+# if isinstance(x,list):
+#     print("yes")
+# else:
+#     print("no")
+y = 10// 1
+z = 10 /1
+print(type(z))
+print(type(y))
 
 
